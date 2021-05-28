8dda61e21979eba69863f02588bf4d71b4ceaa2c
Jeff <jeffbaxter@hatfieldmedia.com>
2021-05-28 17:15:21 -0400

    adjusted iteration for desktop scripts

--- ci/run_lambdatest.sh
+++ ci/run_lambdatest.sh
@@ -2,6 +2,6 @@
 
 REMOTE_URL="https://$LT_USERNAME:$LT_ACCESS_KEY@hub.lambdatest.com/wd/hub"
 
-FILTER="tests/test_desktop_scroll_screenshot.py"
+FILTER="tests/test_demo.py"
 
 pytest $FILTER --remote_url=$REMOTE_URL
