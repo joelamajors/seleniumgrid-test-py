cd3e81e67376a38364f260ce0f006154b4de3e41
Jeff Baxter <jeffbaxter@hatfieldmedia.com>
2021-08-10 18:18:30 +0000

    adjusted script

--- scripts/lambdatest-screenshots.sh
+++ scripts/lambdatest-screenshots.sh
@@ -4,6 +4,10 @@
 #  - Download the updated S3 bucket file via AWS CLI
 #  - Run the Python command to start the tests
 
+cd /home/ssm-user/scripts/lambdatest-py
+
+source /venv/bin/activate
+
 aws s3 cp s3://lambdatest-urls/urls.json /home/ssm-user/scripts/lambdatest-py/urls.json
 
 pytest -s /home/ssm-user/scripts/lambdatest-py/tests/screenshot/take_remote_screenshot.py
