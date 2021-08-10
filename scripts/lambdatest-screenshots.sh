c8c428c577cbd5b45270fe641046515c4f7b7c59
Jeff Baxter <jeffbaxter@hatfieldmedia.com>
2021-08-10 18:14:42 +0000

    removing echos for final testing

--- scripts/lambdatest-screenshots.sh
+++ scripts/lambdatest-screenshots.sh
@@ -4,6 +4,6 @@
 #  - Download the updated S3 bucket file via AWS CLI
 #  - Run the Python command to start the tests
 
-echo 'aws s3 cp s3://lambdatest-urls/urls.json /home/ssm-user/scripts/lambdatest-py/urls.json'
+aws s3 cp s3://lambdatest-urls/urls.json /home/ssm-user/scripts/lambdatest-py/urls.json
 
-echo 'pytest -s /home/ssm-user/scripts/lambdatest-py/tests/screenshot/take_remote_screenshot.py'
+pytest -s /home/ssm-user/scripts/lambdatest-py/tests/screenshot/take_remote_screenshot.py
